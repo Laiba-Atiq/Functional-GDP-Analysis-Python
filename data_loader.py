294898c29629d24a552dbece274c4d70b6c423f0
Laiab-Atiq <laibatiq@gmail.com>
2026-02-07 00:58:47 +0500

    error handling of csv file

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,10 @@ def loadGDPfile(dataFilePath):
     try:
         data = pd.read_csv(dataFilePath)
         return data
-        
+
     except FileNotFoundError:
         raise FileNotFoundError(f"Error: File '{dataFilePath}' not found.")
+        
+    except Exception as exp
+        raise Exception(f"Error : {e} in loading the {dataFilePath} file")
         
