804eff89e21168b886dae22e0a757499ced6a480
Mina Qayyum <minaqayyum06@gmail.com>
2026-02-07 22:07:15 +0500

    sum gdp for regions

--- data_statistics.py
+++ data_statistics.py
@@ -8,6 +8,13 @@ def sumGdp(dfc, dfr):
   #assign adds the sum in data frame where as drop then removes all year columns
   resultCountry = dfc.assign(Sum_of_GDP=dfc[yearColumns].sum(axis=1))
   resultCountry.drop(columns=yearColumns, inplace=True)
+  
+  ########For regions########
+  yearColumnsRegions = list(filter(str.isdigit, dfr.columns))
 
-  return resultCountry
+  #group data on the basis of region, then for all the rows only sum the year columns
+  #the group label is then set as a column value along with year columns
+  resultRegions = (dfc.groupby("Continent", as_index=False)[yearColumnsRegions].sum())
+
+  return resultCountry,resultRegions
   
