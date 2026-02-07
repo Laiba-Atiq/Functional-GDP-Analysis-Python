036ad0f2f80e00a914ac825c0ea350e3265b0934
Laiab-Atiq <laibatiq@gmail.com>
2026-02-07 12:23:02 +0500

    validated data types

--- config_validator.py
+++ config_validator.py
@@ -7,8 +7,25 @@ def validateConfigSchema(configDict):
         missing = list(filter(lamda k: k not in config, keyList))
         raise ValueError(f"Missing keys {missing} in config.json")
 
-    return configDict
+    #validating data types
+    if not isinstance(configDict["region"], str):
+        raise TypeError("Error: region in config.json must be a string")
 
+    if not isinstance(configDict["year"], int):
+        raise TypeError("Error: year in config.json must be an integer")
+
+    if not isinstance(configDict["country"], str):
+        raise TypeError("Error: country in config.json must be an integer")
+
+    if not isinstance(configDict["operation"], str):
+        raise TypeError("Error: operation in config.json must be an integer")
     
+    if not isinstance(configDict["output"], str):
+        raise TypeError("Error: output in config.json must be an integer")
+    
+    
+
+
+
 
 
