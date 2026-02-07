14fe1bf871415cdbc1c3df2097b1a9a887195f6b
Mina Qayyum <minaqayyum06@gmail.com>
2026-02-07 14:24:01 +0500

    country and region checks

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 def dataCleaner(df):
+    #checks if country is valid; df=df[mask]
+    df["Country Name"]=df["Country Name"].str.strip()
+    df = df[ (df["Country"]!= "") & df["Country"].notna() & df["Country"].apply(lambda x: isinstance(x, str)) ]
+
+    #checks if region is valid; df=df[mask]
+    df["Continent"]=df["Continent"].str.strip()
+    validContinents = {"Africa","Asia","Europe","North America","South America","Oceania","Antarctica", "Global"}
+    df = df[df["Continent"].notna() & df["Continent"].isin(validContinents)]
 
     #column names in csv files are string 
     yearCols = list(map(str,range(1960,2025)))
