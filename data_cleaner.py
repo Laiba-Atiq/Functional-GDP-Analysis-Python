1c869f4f24702d0c6a6cb4f8b9c69ac3d2a58f2c
Mina Qayyum <minaqayyum06@gmail.com>
2026-02-08 14:25:24 +0500

    data cleaning and config reading-dashboard

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit as st
 
 def dataCleaner(df):
 
@@ -9,7 +10,7 @@ def dataCleaner(df):
     df["Country Name"]=df["Country Name"].str.strip()
     df = df[ (df["Country Name"]!= "") & df["Country Name"].notna() & df["Country Name"].apply(lambda x: isinstance(x, str)) ]
     
-    print("Country Name invalid for", before-len(df), "rows")
+    st.write("Country Name invalid for", before-len(df), "rows")
     before=len(df)
 
     #checks if region is valid; df=df[mask]
@@ -17,7 +18,7 @@ def dataCleaner(df):
     validContinents = {"Africa","Asia","Europe","North America","South America","Oceania","Antarctica", "Global"}
     df = df[df["Continent"].notna() & df["Continent"].isin(validContinents)]
 
-    print("Continent invalid for", before-len(df), "rows")
+    st.write("Continent invalid for", before-len(df), "rows")
     before=len(df)
 
     #checks
@@ -25,7 +26,7 @@ def dataCleaner(df):
             df["Indicator Name"].apply(lambda x: isinstance(x, str)) &
             df["Indicator Code"].apply(lambda x: isinstance(x, str))]
     
-    print("Country Code/Indicator Name/Indicator Code invalid for", before-len(df), "rows")
+    st.write("Country Code/Indicator Name/Indicator Code invalid for", before-len(df), "rows")
     before=len(df)
 
     #column names in csv files are string 
@@ -38,7 +39,7 @@ def dataCleaner(df):
     #selects only the year cols and check if an entire row is NaN then removes it
     df = df.dropna(subset=yearCols, how='all')
 
-    print("All GDP years empty for", before-len(df), "rows")
+    st.write("All GDP years empty for", before-len(df), "rows")
     before=len(df)
 
     #df.to_csv("cleaned_gdp_data.csv", index=False) #; to check the data exported 
--- main.py
+++ main.py
@@ -2,19 +2,28 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import time
 from data_loader import loadGDPfile
+from config_reader import readConfigFile
+from data_cleaner import dataCleaner
 
 filePath="gdp_with_continent_filled.csv"
 
 #setting up dashboard
 st.set_page_config(page_title="GDP Analysis",page_icon=":bar_chart:",layout="wide")
 
-#loading gdp data in data frame
-try:
-    with st.spinner("Loading data..."):
-        time.sleep(1)
-        loadedData=loadGDPfile(filePath)
-        st.success("Data loaded successfully")
-except Exception as exp:
-    st.error("Data loading failed")
-    st.exception(exp)
-    st.stop()
+with st.status("Initializing dashboard...", expanded=True) as status:
+    try:
+        loadedData = loadGDPfile(filePath)
+        st.write("✅ GDP data loaded")
+
+        cleanedData=dataCleaner(loadedData)
+        st.write("✅ GDP data cleaned")
+
+        configDictionary = readConfigFile()
+        st.write("✅ Configuration file loaded")
+
+        status.update(label="Initialization complete", state="complete")
+
+    except Exception as exp:
+        status.update(label="Initialization failed", state="error")
+        st.exception(exp)
+        st.stop()
